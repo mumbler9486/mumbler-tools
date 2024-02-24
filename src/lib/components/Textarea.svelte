2086a70296e9aa1f4092b07dc189eebc33ba659d
kevinta893 <mumbler9486@gmail.com>
2024-02-24 10:10:01 +0900

    ✨Allow reverse direction json list editing

--- src/lib/components/Textarea.svelte
+++ src/lib/components/Textarea.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	export let label: string;
+	export let error: string = '';
 	export let placeholder: string = '';
 	export let rows: number = 4;
 	export let value: string = '';
@@ -9,6 +10,6 @@
 	<div class="label">
 		<span class="label-text">{label}</span>
 	</div>
-	<textarea {rows} class="textarea textarea-bordered" {placeholder} bind:value></textarea>
-	<div class="label"></div>
+	<textarea {rows} class="textarea textarea-bordered" {placeholder} bind:value on:input />
+	<div class="label text-xs text-error">{error}</div>
 </label>
--- src/routes/tools/text/json-list-editor/+page.svelte
+++ src/routes/tools/text/json-list-editor/+page.svelte
@@ -2,26 +2,76 @@
 	import Textarea from '$lib/components/Textarea.svelte';
 
 	let list: string = '';
-	let jsonList: string = '';
+	let jsonList: string = '[]';
 
-	$: {
-		const itemList = list.split('\n');
+	let isTextFocused: boolean = false;
+	let isJsonFocused: boolean = false;
+
+	let jsonError: string = '';
+
+	$: convertToJson(list);
+	$: convertToText(jsonList);
+
+	const convertToJson = (rawList: string) => {
+		console.log('text');
+
+		// List to JSON
+		if (!isTextFocused) {
+			return;
+		}
+
+		const itemList = rawList.split('\n');
 
 		if (itemList.length > 0 && itemList[0] === '') {
 			jsonList = '[]';
 		} else {
 			jsonList = itemList.length > 0 && itemList[0] ? JSON.stringify(itemList, null, 2) : '[]';
 		}
-	}
+		jsonError = '';
+	};
+
+	const convertToText = (rawJson: string) => {
+		// JSON to list
+		if (!isJsonFocused) {
+			return;
+		}
+
+		try {
+			const listRaw = JSON.parse(rawJson) as string[];
+			list = listRaw.join('\n');
+			jsonError = '';
+		} catch (err) {
+			jsonError = (err as Error).toString();
+			console.error(err);
+		}
+	};
+
+	const textInputPressed = () => {
+		isTextFocused = true;
+		isJsonFocused = false;
+	};
+
+	const jsonInputPressed = () => {
+		isTextFocused = false;
+		isJsonFocused = true;
+	};
 </script>
 
 <div class="flex w-full">
 	<Textarea
 		class="w-1/2"
 		rows={18}
-		label="Enter a comma separated list of items:"
+		label="Enter a newline separated list of items:"
 		bind:value={list}
+		on:input={textInputPressed}
 	/>
 	<div class="divider divider-horizontal"></div>
-	<Textarea class="w-1/2" rows={18} label="JSON List" bind:value={jsonList} />
+	<Textarea
+		class="w-1/2"
+		rows={18}
+		label="JSON List"
+		bind:value={jsonList}
+		on:input={jsonInputPressed}
+		error={jsonError}
+	/>
 </div>
